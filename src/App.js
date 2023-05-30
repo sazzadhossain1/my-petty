edbb625bedb572026cda978a6339bb544f5a912c
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-30 18:20:12 +0600

    add nav bar in the ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Main from "./LayOut/Main";
 import Home from "./conponents/Home/Home";
+import About from "./conponents/About/About";
+import Service from "./conponents/Service/Service";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,6 +14,18 @@ function App() {
         {
           path: "/",
           element: <Home></Home>,
+        },
+        {
+          path: "/home",
+          element: <Home></Home>,
+        },
+        {
+          path: "/about",
+          element: <About></About>,
+        },
+        {
+          path: "/service",
+          element: <Service></Service>,
         },
       ],
     },
---
+++ src/conponents/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is about</h1>
+    </div>
+  );
+};
+
+export default About;
