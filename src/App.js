13e9b1db0d8aa19cc0ae5043e2172c73c8fff7b0
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-30 16:41:19 +0600

    create some components and install react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from "./logo.svg";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Main from "./LayOut/Main";
+import Home from "./conponents/Home/Home";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <h1>this is app</h1>
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
