09a701b89b39ee5d44b980c13050d621e269e88e
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-05 18:39:41 +0600

    add api's info and responsive ness

--- src/conponents/SingleExprt/SingleExprt.js
+++ src/conponents/SingleExprt/SingleExprt.js
@@ -29,11 +29,74 @@ const SingleExprt = () => {
         </Link>
       </div>
 
-      <div>
+      <div className="exprt-exprience-grid-div">
         <img className="singlePhoto" src={img} alt="" />
         <div className="exprtDetails-div">
           <h2 className="personName">{personName}</h2>
           <p className="instructor">{instructor}</p>
+          <div></div>
+          <div></div>
+          <hr />
+          <p className="exprt-exprience-name">
+            Position : <span>{position}</span>
+          </p>
+          <p className="exprt-exprience-name">
+            Experience : <span>{experience}</span>
+          </p>
+          <p className="exprt-exprience-name">
+            Email : <span>{email}</span>
+          </p>
+          <p className="exprt-exprience-name">
+            Phone : <span>{phone}</span>
+          </p>
+          <hr />
+          <div className="share-link-div">
+            <h3 className="share-link">Share Link :</h3>
+            <Link
+              to="https://www.facebook.com/"
+              target="_blank"
+              className="link"
+            >
+              <img
+                className="exprt-logo-png"
+                src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo-500x313.png"
+                alt=""
+              />
+            </Link>
+            <Link
+              to="https://www.facebook.com/"
+              target="_blank"
+              className="link"
+            >
+              <img
+                className="exprt-logo-png"
+                src="https://play-lh.googleusercontent.com/VRMWkE5p3CkWhJs6nv-9ZsLAs1QOg5ob1_3qg-rckwYW7yp1fMrYZqnEFpk0IoVP4LM=w240-h480-rw"
+                alt=""
+              />
+            </Link>
+            <Link
+              to="https://www.facebook.com/"
+              target="_blank"
+              className="link"
+            >
+              <img
+                className="exprt-logo-png"
+                src="https://w7.pngwing.com/pngs/708/311/png-transparent-icon-logo-twitter-logo-twitter-logo-blue-social-media-area-thumbnail.png"
+                alt=""
+              />
+            </Link>
+            <Link
+              to="https://www.facebook.com/"
+              target="_blank"
+              className="link"
+            >
+              <img
+                className="exprt-logo-png"
+                src="https://play-lh.googleusercontent.com/VRMWkE5p3CkWhJs6nv-9ZsLAs1QOg5ob1_3qg-rckwYW7yp1fMrYZqnEFpk0IoVP4LM=w240-h480-rw"
+                alt=""
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
