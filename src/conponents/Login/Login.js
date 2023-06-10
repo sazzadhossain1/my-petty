4a034edc4c92607413be457f9e2a089e088a6412
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-10 16:38:31 +0600

    add some style in register and login component

--- src/conponents/Login/Login.js
+++ src/conponents/Login/Login.js
@@ -1,9 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
-      <h1>This is login</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-6xl font-bold my-6">Please Login!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  className="input-filed"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input-filed"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input-filed"
+                  required
+                />
+              </div>
+
+              <h1 className="text-center my-3">
+                New to this site ?
+                <Link
+                  style={{ color: "green", marginLeft: "10px" }}
+                  to="/register"
+                >
+                  Please Register
+                </Link>
+              </h1>
+              <p
+                style={{ color: "red" }}
+                className="successAndError text-center "
+              ></p>
+
+              <p
+                style={{ color: "green" }}
+                className="successAndError text-center"
+              >
+                User Login Successfully
+              </p>
+
+              <div className="form-control mt-6">
+                <button className="register-btn">Login</button>
+              </div>
+            </form>
+
+            <button className="googleBtn ">
+              <p className="ml-5">SignIn With Google</p>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
