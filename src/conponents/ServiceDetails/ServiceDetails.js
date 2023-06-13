b7e7a016c2ba03641d499d77c94786a131e56e34
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-13 20:03:36 +0600

    get api from vercel and show api by id in service details component

--- src/conponents/ServiceDetails/ServiceDetails.js
+++ src/conponents/ServiceDetails/ServiceDetails.js
@@ -5,6 +5,21 @@ import { Link, useLoaderData } from "react-router-dom";
 const ServiceDetails = () => {
   const getServiceDataById = useLoaderData();
   console.log(getServiceDataById);
+  const {
+    img2,
+    descriptionImg,
+    boardingParagraph,
+    bording,
+    counseling,
+    description2,
+    description3,
+    description4,
+    description5,
+    food,
+    habilitation,
+    provide,
+  } = getServiceDataById;
+
   return (
     <div>
       <div className="serviceDetails-parent-div">
@@ -19,6 +34,47 @@ const ServiceDetails = () => {
         </h2>
       </div>
       <h1>This is service Details</h1>
+      <div className="sectionAndAside-parent container">
+        <section className="left-site-section">
+          <img className="highlight-photo" src={img2} alt="" />
+          <h2>{provide}</h2>
+          <p>{description2}</p>
+          <img src={descriptionImg} alt="" />
+          <h1>{bording}</h1>
+          <p>{description3}</p>
+          <ul>
+            <p>{description4}</p>
+            <li>{counseling}</li>
+            <li>{habilitation}</li>
+            <li>{food}</li>
+          </ul>
+          <p>{description5}</p>
+        </section>
+        <aside className="right-site-section">
+          <div>
+            <h2>Search</h2>
+            <div></div>
+            <div></div>
+            <input type="text" placeholder="Search Keyword" />
+          </div>
+          <div>
+            <h2>Categories</h2>
+            <div></div>
+            <div></div>
+            <Link>Pet Grooming</Link>
+            <br />
+            <Link>Pet Food Provider</Link>
+            <br />
+            <Link>Veterinary Service</Link>
+            <br />
+            <Link>Healthy Meals</Link>
+            <br />
+            <Link>Healthy Pet Food</Link>
+            <br />
+            <Link>Pet Care</Link>
+          </div>
+        </aside>
+      </div>
     </div>
   );
 };
